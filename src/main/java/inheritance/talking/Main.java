989645d81be61ef8643a89de6062110b80391aa9
even-ewgen <ewgenia1997@gmail.com>
2020-03-12 20:22:09 +0500

    С подсчетом кошек и собак.

--- src/main/java/inheritance/talking/Main.java
+++ src/main/java/inheritance/talking/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     public static void main(String[] arg) {
         Animal[] animals = {new Bee(), new Cat(), new Dog(), new Sheep(), new Cat()};
+        int counter = 0;
         for (Animal animal : animals) {
+            if (animal instanceof Dog || animal instanceof Cat) {
+                counter++;
+            }
             animal.talk();
         }
+        System.out.printf("Кошек и собак: %d\n", counter);
     }
 }

cd5b41062e93f62d02d791bfd5c6d27694d8549d
even-ewgen <ewgenia1997@gmail.com>
2020-03-12 20:17:33 +0500

    Пример регулярных выражений с занятия.

---
+++ src/main/java/str/Reg.java
@@ -0,0 +1,21 @@
+package str;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reg {
+
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        Pattern pattern = Pattern.compile("\\+7\\(\\d{3}\\)\\d{3}(-\\d{2}){2}");
+        Matcher matcher = pattern.matcher(s);
+        if (matcher.find()) {
+            System.out.printf("Введен номер: %s\n", matcher.group());
+        } else {
+            System.out.println("Не то.");
+        }
+    }
+}

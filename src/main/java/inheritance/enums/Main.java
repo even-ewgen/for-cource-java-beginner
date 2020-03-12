121313954d7a1687498de0a4e3110669c4ade84e
even-ewgen <ewgenia1997@gmail.com>
2020-03-12 20:38:40 +0500

    Пример enum с занятия.

---
+++ src/main/java/inheritance/enums/Main.java
@@ -0,0 +1,9 @@
+package inheritance.enums;
+
+
+public class Main {
+
+    public static void main(String[] arg) {
+        System.out.println(Operation.apply(Operation.DIVIDE, 10, 5));
+    }
+}
---
+++ src/main/java/inheritance/enums/Operation.java
@@ -0,0 +1,18 @@
+package inheritance.enums;
+
+public enum Operation {
+    PLUS   { double eval(double x, double y)
+    { return x + y; } },
+    MINUS  { double eval(double x, double y)
+    { return x - y; } },
+    TIMES  { double eval(double x, double y)
+    { return x * y; } },
+    DIVIDE { double eval(double x, double y)
+    { return x / y; } };
+
+    abstract double eval(double x, double y);
+
+    public static double apply(Operation op, double x, double y) {
+        return op.eval(x, y);
+    }
+}
